48ed694024aefcfe6a7e41b80d57ea2a8a114037
josep <jcminformatica98@gmail.com>
2019-10-03 17:50:29 +0200

    Android

---
+++ Android/Activitat3/app/src/main/java/com/example/activitat3/Ejercicio1.java
@@ -0,0 +1,29 @@
+package com.example.activitat3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Ejercicio1 extends AppCompatActivity implements View.OnClickListener{
+    TextView txt1;
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame);
+        b1= (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(this);
+        txt1 = (TextView) findViewById(R.id.textView1);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        txt1.setVisibility(View.VISIBLE);
+        b1.setVisibility(View.INVISIBLE);
+    }
+}
