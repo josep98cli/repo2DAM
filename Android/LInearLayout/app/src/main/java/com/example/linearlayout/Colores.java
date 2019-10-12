d48142e4f63c9e6335d138c624f3d1ae8a79031d
Josep <jcminformatica98@gmail.com>
2019-10-12 16:47:27 +0200

    Android Colors Final

--- Android/LInearLayout/app/src/main/java/com/example/linearlayout/Colores.java
+++ Android/LInearLayout/app/src/main/java/com/example/linearlayout/Colores.java
@@ -1,11 +1,13 @@
 package com.example.linearlayout;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -15,48 +17,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Colores extends AppCompatActivity implements View.OnClickListener{
 
-    public static final String TAG = "hola";
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
 
-
         CheckBox checkbox = findViewById(R.id.checkTxt);
-
-
-
         checkbox.setOnClickListener(this);
-
 
     }
 
     @Override
     public void onClick(View view) {
 
-        CheckBox check1 = findViewById(R.id.checkTxt);
         RadioGroup radioTexto = findViewById(R.id.colorTexto);
         RadioGroup radioFondo = findViewById(R.id.radioFondo);
 
+        radioFondo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                CheckBox checkbox = findViewById(R.id.checkTxt);
+                TextView txtView= findViewById(R.id.txtViewColors);
 
-        RadioButton fondoNegro = findViewById(R.id.colorNegro);
-        int fondoNegroId = radioFondo.getId();
-        RadioButton fondoVerde = findViewById(R.id.colorVerde);
-        RadioButton fondoRojo = findViewById(R.id.colorRojo);
+                if (checkbox.isChecked()){
+                    if (i == R.id.colorNegro){
+                        txtView.setBackgroundColor(Color.BLACK);
+                    }else if(i == R.id.colorRojo){
+                        txtView.setBackgroundColor(Color.RED);
+                    }else if(i == R.id.colorVerde){
+                        txtView.setBackgroundColor(Color.GREEN);
+                    }
+                }
 
-        RadioButton textoNegro = findViewById(R.id.textoNegro);
-        RadioButton textoRojo = findViewById(R.id.textoRojo);
-        RadioButton textoVerde = findViewById(R.id.textoVerde);
+            }
+        });
 
-        TextView txtView = findViewById(R.id.txtView);
+        radioTexto.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                TextView txtView= findViewById(R.id.txtViewColors);
+                CheckBox checkbox = findViewById(R.id.checkTxt);
 
+                if (checkbox.isChecked()){
+                    if (i == R.id.textoNegro){
+                        txtView.setTextColor(Color.BLACK);
+                    }else if(i == R.id.textoRojo){
+                        txtView.setTextColor(Color.RED);
+                    }else if(i == R.id.textoVerde){
+                        txtView.setTextColor(Color.GREEN);
+                    }
+                }
 
-        if (check1.isChecked()){
-
-        }
+            }
+        });
     }
 }
---
+++ Android/LInearLayout/app/src/main/java/com/example/linearlayout/Puzle.java
@@ -0,0 +1,15 @@
+package com.example.linearlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Puzle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzle);
+
+    }
+}
