46212a90d984b9bf7af1a7b592a41d95925a0ef2
Josep <jcminformatica98@gmail.com>
2019-10-13 16:58:52 +0200

    Acces a dades v.1

--- Acces a dades/ADT_Reto1JosepCliment/src/main/java/org/simarro/app.java
+++ Acces a dades/ADT_Reto1JosepCliment/src/main/java/org/simarro/app.java
@@ -7,14 +7,11 @@ package org.simarro;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
-import com.drew.imaging.jpeg.JpegMetadataReader;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
-import com.drew.metadata.exif.ExifSubIFDDirectory;
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,7 +21,26 @@ import java.util.logging.Logger;
  * @author vesprada
  */
 public class app {
-
+	
+	
+	public static void crearCarpetas(String fechaMod, File imagen, File rutaDestino) {
+		System.out.println("Ha entrado");
+		String []arrayFecha = fechaMod.split(" ");
+		String nomImagen =	imagen.getPath();
+		
+		String fechaOrdenada = arrayFecha[2]+arrayFecha[1]+arrayFecha[5];
+		File carpetaFecha = new File(rutaDestino + "/" + fechaOrdenada);
+		String destino = rutaDestino.getAbsolutePath()+"/"+fechaOrdenada+"/"+imagen.getName();
+		if (!carpetaFecha.exists()) {
+			if (carpetaFecha.mkdirs()) {
+			imagen.renameTo(new File(destino));
+			}
+		}else {
+			imagen.renameTo(new File(destino));
+		}
+	}
+	
+	
     public static void main(String[] args) {
 
         try {
@@ -35,29 +51,44 @@ public class app {
             //le pido al usuario que introduzca las carpeta de origen y destino
             //System.out.println("Introduzca la carpeta de origen");
             //rutaOrigen = s.next();
-            rutaOrigen = "/home/vesprada/Escriptori/imgOrigen/index.jpeg";
+            rutaOrigen = "img";
 
             //System.out.println("Introduzca la carpeta de destino");
             //rutaDestino = s.next();
-            //rutaDestino = "/home/vesprada/NetBeansProjects/Reto1JosepCliment/imgDestino/";
+            
             //Creo dos files con las rutas que ha introducido el usuario
             File carpetaOrigen = new File(rutaOrigen);
-            //File carpetaDestino = new File(rutaDestino);
-            //Metadata meta = ImageMetadataReader.readMetadata(carpetaOrigen);
+            File carpetaDestino = new File("finImagen");
 
-            Metadata metadata = ImageMetadataReader.readMetadata(carpetaOrigen);
-
-            for (Directory directory : metadata.getDirectories()) {
-                for (Tag tag : directory.getTags()) {
-                    System.out.println(directory.getName() + tag.getTagName() + tag.getDescription());
-                            
-                }
-                if (directory.hasErrors()) {
-                    for (String error : directory.getErrors()) {
-                        System.err.format("ERROR: %s", error);
-                    }
-                }
-            }
+           
+            String fechaMod="";
+            
+            String []arr_res = null;
+            if (carpetaOrigen.isDirectory()) {
+				File[] arr_content = carpetaOrigen.listFiles();
+				
+				for (int i = 0; i < arr_content.length; i++) {
+					if (arr_content[i].isFile()) {
+						
+						 Metadata metadata = ImageMetadataReader.readMetadata(arr_content[i]);
+						 for (Directory directory : metadata.getDirectories()) {
+				                for (Tag tag : directory.getTags()) {
+				                    fechaMod = directory.getDescription(3);  
+				                }
+				              
+				                if (directory.hasErrors()) {
+				                    for (String error : directory.getErrors()) {
+				                        System.err.format("ERROR: %s", error);
+				                    }
+				                }
+				            }
+				           crearCarpetas(fechaMod, arr_content[i],carpetaDestino);
+				           
+					}
+				}
+			}
+            
+            
         } catch (ImageProcessingException ex) {
             Logger.getLogger(app.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
