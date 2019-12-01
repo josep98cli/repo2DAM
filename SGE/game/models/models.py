07e5e6cb900c529aba2b6b09566d24b34dd4c980
josepOdoo <jcminformatica98@gmail.com>
2019-12-01 20:19:33 +0100

    fi autocalcul de produccio
    arreglat la produccio y autocalcul

--- SGE/game/models/models.py
+++ SGE/game/models/models.py
@@ -2,10 +2,12 @@
 
 from odoo import models, fields, api, tools
 import random
+from openerp.exceptions import except_orm
 
 
 class jugador(models.Model):
     _name = 'game.jugador'
+
     # imageJugador = fields.Binary()
     name = fields.Char(string='Nombre jugador',
                        default=lambda self: self._get_default_name(), )
@@ -33,6 +35,20 @@ class jugador(models.Model):
 
         return res
 
+    @api.model
+    def update_recursos(self):
+        jugador_total = self.env['game.jugador'].search([])
+        for j in jugador_total:
+            for r in j.ciutat.recursos:
+                for m in j.ciutat.mines:
+                    print(m.mejora)
+                    if m.mina.recurs.name == r.name:
+                        if m.minutos == 0:
+                            r.cantidad += m.produccion
+                            m.mejora = False
+                        elif m.mejora:
+                            m.write({'minutos': m.minutos - 1})
+
 
 class ciutat(models.Model):
     _name = 'game.ciutat'
@@ -51,7 +67,6 @@ class ciutat(models.Model):
     jugador = fields.Many2one('game.jugador')
     recursos = fields.One2many('game.recursos', 'ciutat')
     mines = fields.One2many('game.mines', 'ciutat')
-    recurs = fields.Many2one('game.recurs')
 
     @api.model
     def create(self, values):
@@ -63,17 +78,21 @@ class ciutat(models.Model):
             r = res.recursos.create({
                 'ciutat': res.id,
                 'recurs': self.env.ref(i).id,
-                'name': self.env.ref(i).name
+                'name': self.env.ref(i).name,
+                'cantidad': 1000
             })
 
         for j in array_mines:
             m = res.mines.create({
                 'ciutat': res.id,
                 'mina': self.env.ref(j).id,
-                'name': self.env.ref(j).name
+                'name': self.env.ref(j).name,
+                'coste': 100
             })
 
         return res
+
+    # r.mina.recurs.name
 
 
 class recursos(models.Model):
@@ -84,8 +103,29 @@ class recursos(models.Model):
     cantidad = fields.Float()
 
     @api.multi
-    def calcular_cantidad(self, recurs1):
-        print(self)
+    def calcular_coste(self, recurs1, nivel, produccion, name_mina):
+
+        for r in self:
+            if nivel > 0:
+                coste = (nivel + 2) * ((produccion + 20) * 10)
+            else:
+                coste = 100
+
+            if r.name == recurs1.name:
+                if r.cantidad >= coste:
+                    for m in r.ciutat.mines:
+                        if m.name == name_mina:
+                            m.mejora = True
+                            if m.minutos == 0:
+                                m.nivel += 1
+                                m.produccion += (nivel + 1) * 300
+                                m.minutos = (m.minutos + 1) * 2
+                                m.coste = coste
+                                r.cantidad -= coste
+                    print(m.mejora)
+                else:
+                    raise except_orm('ERROR',
+                                     'Faltan recursos para hacer la mejora')
 
 
 class recurs(models.Model):
@@ -95,7 +135,6 @@ class recurs(models.Model):
     cantidad = fields.Float()
     mina = fields.One2many('game.mina', 'recurs')
     ciutat = fields.Many2one('game.ciutat', 'recurs')
-    recurs = fields.One2many('game.coste', 'recurs')
 
 
 class mines(models.Model):
@@ -103,24 +142,18 @@ class mines(models.Model):
     name = fields.Char()
     ciutat = fields.Many2one('game.ciutat')
     mina = fields.Many2one('game.mina')
+    coste = fields.Float()
+    produccion = fields.Float()
+    nivel = fields.Integer()
+    minutos = fields.Integer()
+    mejora = fields.Boolean(default=False)
 
     @api.multi
     def llamar_calc_cantidad(self):
-        self.ciutat.recursos.calcular_cantidad(self.mina.recurs)
+        self.ciutat.recursos.calcular_coste(self.mina.recurs, self.nivel, self.minutos, self.name)
 
 
 class mina(models.Model):
     _name = 'game.mina'
     name = fields.Char()
-    nivel = fields.Integer()
-    produccion = fields.Float()
-    recurs = fields.Many2one('game.recurs')
-    minutos = fields.Integer()
-    coste = fields.One2many('game.coste', 'mina')
-
-
-class coste(models.Model):
-    _name = 'game.coste'
-    cantidad = fields.Float()
-    mina = fields.Many2one('game.mina')
     recurs = fields.Many2one('game.recurs')
